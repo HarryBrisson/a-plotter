fe27360d1735786ceeb317d1d3b2b031c02a1d81
HarryBrisson <ejbrisson@gmail.com>
2018-10-13 14:19:46 -0700

    access aframe-based template

---
+++ aplotter.py
@@ -0,0 +1,15 @@
+# getting raw html_template for input
+def get_html_template():
+	with open("template.html") as f:
+		template = f.read()
+	return template
+
+# data: string including list of dictionaries for every cube, including links to images
+# x: string including name of x variable
+# y: string including name of y variable
+# z: string including name of z variable
+def update_html_template(data,x,y,z):
+	template = template.replace("INSERT_DATA_HERE",data)
+	template = template.replace("INSERT_X_HERE",x)
+	template = template.replace("INSERT_Y_HERE",y)
+	template = template.replace("INSERT_Z_HERE",z)
