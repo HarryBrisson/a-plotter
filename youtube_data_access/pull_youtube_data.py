ad7ede2e92eb4bcec1d3f977024cd9a96b5c4ceb
HarryBrisson <ejbrisson@gmail.com>
2018-10-14 11:22:38 -0700

    get youtube data as d3 string and some refactoring

--- youtube_data_access/pull_youtube_data.py
+++ youtube_data_access/pull_youtube_data.py
@@ -4,17 +4,13 @@ import json
 import pandas as pd
 
 
-# note, want to refactor into neater functions
-
-def get_data_for_youtube_channels(search_term):
-
-    search_term = term
+def get_list_of_youtube_channels(term,n):
 
     # initialize list of links
     links = []
 
     # get a list of links for channels while searching for a given term
-    for i in range(0,200,10):
+    for i in range(0,n,10):
         r = requests.get("https://www.bing.com/search?q="+term+"%20site%3A%20https%3A%2F%2Fwww.youtube.com%2Fchannel%2F&first="+str(i))
         soup = BeautifulSoup(r.text,'html.parser')
         link_elements = soup.find_all('h2')
@@ -32,20 +28,30 @@ def get_data_for_youtube_channels(search_term):
     # remove duplicates
     channel_ids = list(set(channel_ids))
 
+    return channel_ids
+
+
+# get API key for the YouTube Data v3 API -- you can create one at https://console.developers.google.com/apis/credentials
+def get_youtube_api_key():
+
+    with open('authentications/youtube_api_key.txt', 'r') as f:
+        api_key=f.read()
+
+    return api_key
+
+
+def get_data_string_for_youtube_channels(channel_ids):
 
     # initialize dataframe
     df = pd.DataFrame()
 
-    # get API key for the YouTube Data v3 API -- you can create one at https://console.developers.google.com/apis/credentials
-    with open('youtube_api_key.txt', 'r') as f:
-        api_key=f.read()
+    api_key = get_youtube_api_key()
 
     # access youtube stats for each channel id, adding rows to the dataframe
     for c in channel_ids:
         
         r = requests.get("https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id="+c+"&key="+api_key)
         full_data = json.loads(r.text)
-        print(full_data)
         
         # try to get a complete row for as many ids as we can
         try:
@@ -71,3 +77,10 @@ def get_data_for_youtube_channels(search_term):
     d3_data_string = str(df.to_dict('records'))
 
     return d3_data_string
+
+
+def get_youtube_data_string_for_search_term(term,n):
+    channel_ids = get_list_of_youtube_channels(term,n)
+    data_string = get_data_string_for_youtube_channels(channel_ids)
+    return data_string
+
