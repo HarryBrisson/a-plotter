670ba162f7cea8b40eaaf902f36ed0e658772261
HarryBrisson <ejbrisson@gmail.com>
2018-10-14 11:19:15 -0700

    fix missing return

--- aplotter.py
+++ aplotter.py
@@ -9,7 +9,9 @@ def get_html_template():
 # y: string including name of y variable
 # z: string including name of z variable
 def update_html_template(data,x,y,z):
+	template = get_html_template()
 	template = template.replace("INSERT_DATA_HERE",data)
 	template = template.replace("INSERT_X_HERE",x)
 	template = template.replace("INSERT_Y_HERE",y)
 	template = template.replace("INSERT_Z_HERE",z)
+	return template
