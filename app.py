536682b2a47af79e5cd7d6b6a3589fe475ab3aa0
HarryBrisson <ejbrisson@gmail.com>
2018-10-17 20:58:10 -0700

    add temp function for quicker testing

--- app.py
+++ app.py
@@ -22,3 +22,9 @@ def youtube_demo():
 		return update_html_template(data_string,"'videos'","'subs'","'views'")
 	else:
 		return "Coming Soon: Input for Query Terms"
+
+# temporary - clear this after better data access system is set up
+@app.route("/youtube-test")
+def youtube_test():
+	data_string = get_test_youtube_data_string()
+	return update_html_template(data_string,"'videos'","'subs'","'views'")
